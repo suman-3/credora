29032c782cab1c5000d6e012726d82454b2cca8b
suman-3 <mondalsumanrock20@gmail.com>
2025-08-24 11:26:33 +0530

    feat: add Issue Credential page and update navigation items for certificate issuance

---
+++ apps/console/app/dashboard/issue/page.tsx
@@ -0,0 +1,31 @@
+import { Heading } from '@/components/heading'
+import PageContainer from '@/components/layout/page-container'
+import { DataTableSkeleton } from '@/components/table/data-table-skeleton'
+import { Separator } from '@workspace/ui/components/separator'
+import React, { Suspense } from 'react'
+
+const IssuePage = () => {
+  return (
+    <PageContainer scrollable={false}>
+      <div className="flex flex-1 flex-col space-y-4">
+        <div className="flex items-start justify-between">
+          <Heading
+            title="Applications"
+            description="Manage your applications and their settings."
+          />
+        </div>
+        <Separator />
+        <Suspense
+          // key={key}
+          fallback={
+            <DataTableSkeleton columnCount={5} rowCount={8} filterCount={2} />
+          }
+        >
+          <IssuePage />
+        </Suspense>
+      </div>
+    </PageContainer>
+  )
+}
+
+export default IssuePage
